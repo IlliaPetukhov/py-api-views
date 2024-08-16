0a1288b67956d3c94c6f9a6c09ca178aa099c70b
Illia <ilaruslanovich7@gmail.com>
2024-08-16 20:40:42 +0200

    Solution

--- cinema/views.py
+++ cinema/views.py
@@ -7,11 +7,8 @@ from django.shortcuts import get_object_or_404
 from cinema.models import Movie, Genre, Actor, CinemaHall
 from cinema.serializers import (
     MovieSerializer,
-
     GenreSerializer,
-
     ActorSerializer,
-
     CinemaHallSerializer
 )
 
@@ -78,6 +75,18 @@ class ActorDetail(generics.GenericAPIView,
                   mixins.RetrieveModelMixin):
     queryset = Actor.objects.all()
     serializer_class = ActorSerializer
+
+    def get(self, request, *args, **kwargs) -> Response:
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs) -> Response:
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs) -> Response:
+        return self.destroy(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs) -> Response:
+        return self.partial_update(request, *args, **kwargs)
 
 
 class CinemaHallViewSet(mixins.ListModelMixin,
