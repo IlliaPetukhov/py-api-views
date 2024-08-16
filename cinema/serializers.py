817a95898434b85e21689715e6988544c0544504
Illia <ilaruslanovich7@gmail.com>
2024-08-16 14:57:45 +0200

    Solution

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from cinema.models import Movie
+from cinema.models import Movie, Actor, CinemaHall, Genre
 
 
 class MovieSerializer(serializers.Serializer):
@@ -8,6 +8,10 @@ class MovieSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=255)
     description = serializers.CharField()
     duration = serializers.IntegerField()
+    actors = serializers.PrimaryKeyRelatedField(many=True,
+                                                queryset=Actor.objects.all())
+    genres = serializers.PrimaryKeyRelatedField(many=True,
+                                                queryset=Genre.objects.all())
 
     def create(self, validated_data):
         return Movie.objects.create(**validated_data)
@@ -15,10 +19,59 @@ class MovieSerializer(serializers.Serializer):
     def update(self, instance, validated_data):
         instance.title = validated_data.get("title", instance.title)
         instance.description = validated_data.get(
-            "description", instance.description
-        )
+            "description", instance.description)
         instance.duration = validated_data.get("duration", instance.duration)
-
         instance.save()
+        return instance
 
+
+class GenreSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(max_length=255)
+
+    def create(self, validated_data):
+        return Genre.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get("name", instance.name)
+        instance.save()
+        return instance
+
+    def validate_name(self, value):
+        if Genre.objects.filter(name=value).exists():
+            raise (serializers.ValidationError
+                   ("Genre with this name already exists."))
+        return value
+
+
+class ActorSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    first_name = serializers.CharField(max_length=255)
+    last_name = serializers.CharField(max_length=255)
+
+    def create(self, validated_data):
+        return Actor.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.first_name = (validated_data.get
+                               ("first_name", instance.first_name))
+        instance.last_name = (validated_data.get
+                              ("last_name", instance.last_name))
+        instance.save()
+        return instance
+
+
+class CinemaHallSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(max_length=255)
+    rows = serializers.IntegerField()
+    seats_in_row = serializers.IntegerField()
+
+    def create(self, validated_data):
+        return CinemaHall.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get("name", instance.name)
+        instance.rows = validated_data.get("rows", instance.rows)
+        instance.save()
         return instance
--- cinema/urls.py
+++ cinema/urls.py
@@ -1,10 +1,45 @@
-from django.urls import path
+from django.urls import path, include
 
-from cinema.views import movie_list, movie_detail
+from rest_framework import routers
+
+from cinema.views import (GenreViewList,
+                          GenreViewDetail,
+                          ActorViewList,
+                          ActorViewDetail,
+                          CinemaHallViewSet,
+                          MovieViewSet)
+
+
+cinema_hall_list = CinemaHallViewSet.as_view(actions={"get": "list",
+                                                      "post": "create"})
+
+cinema_hall_detail = (CinemaHallViewSet.as_view
+                      (actions={"get": "retrieve",
+                                "put": "update",
+                                "patch": "partial_update",
+                                "delete": "destroy"}))
+
+router = routers.DefaultRouter()
+
+router.register("movie", MovieViewSet, basename="movie")
+
+# use /movie/ for get or post amd use  /movie/pk/ for delete, update etc...
 
 urlpatterns = [
-    path("movies/", movie_list, name="movie-list"),
-    path("movies/<int:pk>/", movie_detail, name="movie-detail"),
+    path("genres/", GenreViewList.as_view(),
+         name="genres-list"),
+    path("genres/<int:pk>/", GenreViewDetail.as_view(),
+         name="genres-detail"),
+    path("actors/", ActorViewList.as_view(),
+         name="actors-list"),
+    path("actors/<int:pk>/", ActorViewDetail.as_view(),
+         name="actor-detail"),
+    path("cinema-halls/", cinema_hall_list,
+         name="cinema-halls-list"),
+    path("cinema-halls/<int:pk>/", cinema_hall_detail,
+         name="cinema-hall-detail"),
+    path("", include(router.urls)),
+
 ]
 
 app_name = "cinema"
